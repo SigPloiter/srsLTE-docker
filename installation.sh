aec2d57df03a56acb2459b7d3891fa0bde0bdd48
Loay Abdelrazek <SigPloiter@users.noreply.github.com>
2019-07-26 00:56:04 +0200

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -11,11 +11,11 @@ echo " "
 echo "Firing up the containers"
 echo "========================"
 echo " "
-sudo docker-compose up -d
+docker-compose up -d
 
 echo " "
 echo "Allowing container communication in IPtables"
 echo "============================================="
 echo " "
-sudo iptables -I FORWARD 1 -j ACCEPT
-sudo iptables-save
+iptables -I FORWARD 1 -j ACCEPT
+iptables-save
